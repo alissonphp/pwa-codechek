6021a7ee2e4383ca274d347ceb7c4c42d8361467
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2022-12-06 19:37:21 -0300

    feat: on boarding - add tutorial video and pagination backgroundColor

--- src/pages/OnBoarding/index.tsx
+++ src/pages/OnBoarding/index.tsx
@@ -8,9 +8,10 @@ import {
 } from "@chakra-ui/react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { useNavigate } from "react-router-dom";
-import logo from '../../assets/images/logo-default.png'
+import tutorial from '../../assets/videos/tutorial.mp4'
 import board2 from '../../assets/images/onBoarding-2.svg'
 import '@splidejs/react-splide/css';
+import './styles.modules.css'
 
 export const OnBoarding = () => {
   let navigate = useNavigate();
@@ -30,20 +31,22 @@ export const OnBoarding = () => {
         arrows: false,
       }}>
         <SplideSlide>
-          <Center maxW={"sm"} flexDirection='column' height={"100vh"}>
-            <Image src={logo} mx={"auto"} />
-            <Heading fontSize={"lg"} textAlign={'center'} p={"4"} my={"2"}>
-              Gere um QR code e escaneie
-            </Heading>
-            <Text fontSize={"md"} maxW={'80%'} align={"center"} p={"4"} mb={"4"}>
-              A leitura do QR code garante sua presença em aula. O leitor fica em sua sala de aula.
-            </Text>
+          <Center flexDirection='column' height={"100vh"}>
+            <video src={tutorial} autoPlay controls/>
+            <Center flexDirection={'column'} height={'100vh'}>
+              <Heading fontSize={"lg"} textAlign={'center'} p={"4"} my={"2"}>
+                Gere um QR code e escaneie
+              </Heading>
+              <Text fontSize={"md"} maxW={'80%'} align={"center"} p={"4"}>
+                A leitura do QR code garante sua presença em aula. O leitor fica em sua sala de aula.
+              </Text>
+            </Center>
           </Center>
         </SplideSlide>
         <SplideSlide>
-          <Center maxW={"sm"} flexDirection='column' height={"100vh"}>
+          <Center flexDirection='column' height={"100vh"}>
             <Image src={board2} mx={"auto"} />
-            <Heading fontSize={"lg"} textAlign={'center'} my={"8"} mb={'4'}>
+            <Heading fontSize={"lg"} textAlign={'center'} my={"8"}>
               Consulte sua frequência
             </Heading>
             <Text fontSize={"md"} maxW={'80%'} align={"center"}>
